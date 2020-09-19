882992fa685f5acabaae6fbffb0cf9aa684cec48
Stevhen <stevhen.work@gmail.com>
2020-09-19 19:33:53 +0700

    Initial Commit

---
+++ NewsApi/NewsApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApiApp.swift
+//  NewsApi
+//
+//  Created by Stevhen on 19/09/20.
+//
+
+import SwiftUI
+
+@main
+struct NewsApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
